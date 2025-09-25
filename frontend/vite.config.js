fcdeaaaf86b7e30f9a50661c237d3631250b9c9d
Charles Anderson <andersonoyoo034@gmail.com>
2025-09-26 02:46:41 +0300

    fix;Enable sourcemaps for debugging build.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,9 @@
   import react from '@vitejs/plugin-react'
 
   // https://vite.dev/config/
-  export default defineConfig({
-    plugins: [react()],
-  })
+ export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+});
