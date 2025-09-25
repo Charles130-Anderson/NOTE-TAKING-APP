a4986f185b6c0edd3d3df1c3570133e9ae5f37a6
Charles Anderson <andersonoyoo034@gmail.com>
2025-09-25 19:35:10 +0300

    feat: add routing and protected routes in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,44 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import NoteEditor from "./pages/NoteEditor";
-import SharedNotes from "./pages/SharedNotes";
+  import { BrowserRouter, Routes, Route } from "react-router-dom";
+  import Navbar from "./components/Navbar";
+  import ProtectedRoute from "./components/ProtectedRoute";
+  import Login from "./pages/Login";
+  import Register from "./pages/Register";
+  import Dashboard from "./pages/Dashboard";
+  import NoteEditor from "./pages/NoteEditor";
+  import SharedNotes from "./pages/SharedNotes";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/shared"
-          element={
-            <ProtectedRoute>
-              <SharedNotes />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/edit/:id"
-          element={
-            <ProtectedRoute>
-              <NoteEditor />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+  export default function App() {
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/shared"
+            element={
+              <ProtectedRoute>
+                <SharedNotes />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/edit/:id"
+            element={
+              <ProtectedRoute>
+                <NoteEditor />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
