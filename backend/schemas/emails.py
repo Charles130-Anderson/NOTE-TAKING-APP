b6dd9b04da337aedd3cc7a5bb50f52d9a0360c58
Charles130-Anderson <andersonoyoo034@gmail.com>
2025-09-25 07:59:58 -0700

    feat: add email request schema.PEP 8 formatting

--- backend/schemas/emails.py
+++ backend/schemas/emails.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+"""Pydantic schema for sending emails."""
+
 from pydantic import BaseModel, EmailStr
 
+
 class EmailRequest(BaseModel):
+    """Schema for an email request."""
     to: EmailStr
     subject: str
     body: str
