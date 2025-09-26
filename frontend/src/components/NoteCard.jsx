4b45daff2aea83a305c29d635aaf9425de81d886
Charles Anderson <andersonoyoo034@gmail.com>
2025-09-26 09:24:49 +0300

    fix;use env API URL fetch

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -54,7 +54,7 @@
         setStatus(null);
 
         try {
-        const res = await fetch("http://127.0.0.1:8000/send-email", {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/send-email`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
